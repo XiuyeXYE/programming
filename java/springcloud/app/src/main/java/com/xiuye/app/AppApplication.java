20ae61f6a7b76a23b869aa8991eeb0b36fa4c89f
xiuye <xiuye_engineer@outlook.com>
2020-08-25 23:44:22 +0800

    spring cloud

---
+++ java/springcloud/app/src/main/java/com/xiuye/app/AppApplication.java
@@ -0,0 +1,15 @@
+package com.xiuye.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
